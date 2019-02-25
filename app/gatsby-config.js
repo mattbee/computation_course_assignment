cf14e49514fab7dd3cb7217f3cca827b9da2e041
Matt Bee <matt@todayishould.com>
2019-02-25 21:49:51 +0000

    Update the SEO data for the binary app

--- app/gatsby-config.js
+++ app/gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Binary number system learning resource`,
+    description: `Understanding the Binary number system. A worksheet and calulator created for the Demystifying computation course on FutureLearn.`,
+    author: `@mattbee`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
